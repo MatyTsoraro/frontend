e5cb8d1f6d2b53aa157c49e3c84a816ac693a947
MatyTsoraro <99216414+MatyTsoraro@users.noreply.github.com>
2023-02-15 21:52:17 +0200

    Update EditProfilePopup.js

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,69 +1,69 @@
-import React, { useEffect, useContext, useState } from "react";
-import PopupWithForm from "./PopupWithForm";
-import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import React, { useEffect, useContext, useState } from 'react';
+import PopupWithForm from './PopupWithForm';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 const EditProfilePopup = ({ isLoading, isOpen, onClose, onUpdateUser }) => {
   const currentUser = useContext(CurrentUserContext);
 
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
 
   useEffect(() => {
-    setName(currentUser.name || "");
-    setDescription(currentUser.about || "");
-  }, [currentUser]);
-  const handleNameChange = (e) => {
+    setName(currentUser.name || '');
+    setDescription(currentUser.about || '');
+  }, [currentUser, isOpen]);
+  const handleNameChange = e => {
     setName(e.target.value);
   };
-  const handleDescriptionChange = (e) => {
+  const handleDescriptionChange = e => {
     setDescription(e.target.value);
   };
-  // function handleSubmit(e) {
-  //   e.preventDefault();
-  //   onUpdateUser({
-  //     name,
-  //     about: description,
-  //   });
-  // }
+  function handleSubmit(e) {
+    e.preventDefault();
+    onUpdateUser({
+      name,
+      about: description,
+    });
+  }
   return (
     <PopupWithForm
-      title="Edit profile"
-      name="popup-edit-profile"
+      title='Edit profile'
+      name='popup-edit-profile'
       isOpen={isOpen}
       onClose={onClose}
-      // onSubmit={handleSubmit}
-      buttonText={isLoading ? "Saving..." : "Save"}
+      onSubmit={handleSubmit}
+      buttonText={isLoading ? 'Saving...' : 'Save'}
     >
-      <fieldset className="form__fieldset">
-        <div className="form__input-container">
+      <fieldset className='form__fieldset'>
+        <div className='form__input-container'>
           <input
-            id="name-input"
-            type="text"
-            name="name"
+            id='name-input'
+            type='text'
+            name='name'
             value={name}
-            placeholder="Name"
-            className="form__input form__input_type_profile-name"
-            minLength="2"
-            maxLength="40"
+            placeholder='Name'
+            className='form__input form__input_type_profile-name'
+            minLength='2'
+            maxLength='40'
             onChange={handleNameChange}
             required
           />
-          <span className="form__input-error name-input-error" />
+          <span className='form__input-error name-input-error' />
         </div>
-        <div className="form__input-container">
+        <div className='form__input-container'>
           <input
-            id="title-input"
-            type="text"
-            name="title"
-            placeholder="About me"
-            className="form__input form__input_type_profile-title"
-            minLength="2"
-            maxLength="200"
+            id='title-input'
+            type='text'
+            name='title'
+            placeholder='About me'
+            className='form__input form__input_type_profile-title'
+            minLength='2'
+            maxLength='200'
             onChange={handleDescriptionChange}
             required
             value={description}
           />
-          <span className="form__input-error title-input-error" />
+          <span className='form__input-error title-input-error' />
         </div>
       </fieldset>
     </PopupWithForm>
