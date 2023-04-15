396aa93e8004f5f7c24177a518c0a1dbb7d9be02
MatyTsoraro <maty.tsoraro@gmail.com>
2023-04-16 00:18:09 +0300

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,53 @@
-import React from "react";
-import logo from "../images/around.svg";
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
-const Header = () => {
+import logo from '../images/around.svg';
+
+const Header = ({ loggedIn, email, handleSignout }) => {
+  const location = useLocation();
+  const isLogin = location.pathname === '/signin';
+  const isRegister = location.pathname === '/signup';
+
   return (
-    <header className="header">
-      <img src={logo} alt="Around the US Logo" className="header__logo" />
+    <header className='header'>
+      <div className='header__container'>
+        <img src={logo} alt='Around the US Logo' className='header__logo' />
+
+        <nav className='header_navbar'>
+          <ul
+            className={`header__links ${
+              isLogin || isRegister ? 'header__links_signup-login-page' : ''
+            }`}
+          >
+            {isLogin && (
+              <li className='header__link-item'>
+                <Link to='/signup' className='header__link'>
+                  Sign up
+                </Link>
+              </li>
+            )}
+            {isRegister && (
+              <li className='header__link-item'>
+                <Link to='/signin' className='header__link'>
+                  Log in
+                </Link>
+              </li>
+            )}
+            {loggedIn && (
+              <li className='header__link-item'>
+                <Link
+                  to='/signin'
+                  className='header__link'
+                  onClick={handleSignout}
+                >
+                  Log out
+                </Link>
+              </li>
+            )}
+            {loggedIn && <li className='header__link-item'>{email}</li>}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
